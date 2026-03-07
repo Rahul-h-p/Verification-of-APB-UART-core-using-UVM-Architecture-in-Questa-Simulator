91241658f0caf538cb677071e5db4b1beababc9f
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-03-07 21:56:15 +0530

    Create scoreboard.sv

---
+++ tb/scoreboard.sv
@@ -0,0 +1,70 @@
+class scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(scoreboard)
+  env_config m_cfg;
+
+  uvm_tlm_analysis_fifo #(apb_xtn) fifo_w;
+  uvm_tlm_analysis_fifo #(uart_xtn) fifo_r;
+
+  apb_xtn uart1;
+  uart_xtn uart2,uart3;
+  apb_xtn cov_data1;
+  uart_xtn cov_data2;
+  bit[7:0] rx[$], tx[$];
+  int thrlsize, rbrlsize;
+
+  function new(string name="scoreboard", uvm_component parent);
+    super.new(name,parent);
+    apb_signals_cov=new();
+    apb_lcr_cov =new();
+    apb_ier_cov =new();
+    apb_fcr_cov =new();
+    apb_mcr_cov =new();
+    apb_iir_cov =new();
+    apb_lsr_cov =new();
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(env_config)::get(this,"","env_config",m_cfg))
+      `uvm_fatal("CONFIG","cannot get config")
+      fifo_w=new["fifo_w",this];
+    fifo_r=new["fifo_r",this];
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    begin
+      fork
+        forever begin
+          fifo_w.get(uart1);
+          uart1.print;
+          thrlsize=uart1.thr.size;
+          rbrlsize=uart1.rbr.size;
+          `uvm_info("SCOREBOARD",$sformatf("printing from scoreboard of uart1 \n %s", uart1.sprint()),UVM_LOW)
+          cov_data1=uart1;
+        end
+
+        forever begin
+          fifo_r.get(uart2);
+          `uvm_info("SB UART2", "PRINTING UART2 DATA IN SCOREBOARD", UVM_LOW)
+          uart2.print;
+          cov_data2=uart2;
+          `uvm_info("SCOREBOARD", $sformatf("printing from scoreboard of uart2 \n %s", uart2.sprint()),UVM_LOW)
+        end
+      join
+    end
+    apb_signals_cov.sample();
+    apb_lcr_cov.sample();
+
+
+
+          
+  covergroup apb_signals_cov;
+    option.per_instance=1;
+
+    ADDRESS: coverpoint cov_data1.Paddr {bins add={[0:255]};}
+    WR_ENB: coverpoint cov_data1.Pwrite {bins rd={0};
+                                         bins wr={1};}
+  endgroup
+
+  
