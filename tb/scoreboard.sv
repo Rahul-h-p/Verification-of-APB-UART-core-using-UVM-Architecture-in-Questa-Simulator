424cd94421310155c23f710d42aa584516b394bb
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-03-07 22:38:38 +0530

    Update scoreboard.sv

--- tb/scoreboard.sv
+++ tb/scoreboard.sv
@@ -22,7 +22,17 @@ class scoreboard extends uvm_scoreboard;
     apb_iir_cov =new();
     apb_lsr_cov =new();
   endfunction
+        
+  covergroup apb_signals_cov;
+    option.per_instance=1;
 
+    ADDRESS: coverpoint cov_data1.Paddr {bins add={[0:255]};}
+    WR_ENB: coverpoint cov_data1.Pwrite {bins rd={0};
+                                         bins wr={1};}
+  endgroup
+
+
+  
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if(!uvm_config_db#(env_config)::get(this,"","env_config",m_cfg))
@@ -55,16 +65,43 @@ class scoreboard extends uvm_scoreboard;
     end
     apb_signals_cov.sample();
     apb_lcr_cov.sample();
+    apb_ier_cov.sample();
+    apb_fcr_cov.sample();
+    apb_mcr_cov.sample();
+    apb_iir_cov.sample();
+    apb_lsr_cov.sample();
+  endtask
 
+  function void check_phase(uvm_phase phase);
+    `uvm_info(get_type_name(),$sformatf("size of thr 1=%0d", thrlsize),UVM_LOW)
+    `uvm_info(get_type_name(),$sformatf("size of rbr 1=%0d", rbrlsize),UVM_LOW)
+    `uvm_info(get_type_name(),$sformatf("values of uart1 =%p", uart1.thr),UVM_LOW)
+    `uvm_info(get_type_name(),$sformatf("values of uart2 =%p", uart2.tx),UVM_LOW)
+    `uvm_info(get_type_name(),$sformatf("values of uart1 =%p", uart1.rbr),UVM_LOW)
+    `uvm_info(get_type_name(),$sformatf("values of uart2 =%p", uart2.tx),UVM_LOW)
 
+    if((uart1.iir[3:1] ==3'b010)) begin
+      if((uart1.mcr[4] ==0)) begin
+        if(uart1.thr.size()==0) begin
+          if((uart1.Pwdata[7:0]==uart2.rx)||(uart2.tx==uart1.Prdata[7:0]))
+            `uvm_info(get_type_name(),"\n Scoreboard half duplex comparison passed",UVM_LOW)
+            else
+              `uvm_info(get_type_name(),"\n Scoreboard half duplex comparison failed", UVM_LOW)
+              end
+              else begin
+                if((uart1.Pwdata[7:0]==uart2.rx) && (uart2.tx == uart1.Prdata[7:0]))
+                  `uvm_info(get_type_name(),"\n Scoreboard half duplex comparison passed",UVM_LOW)
+            else
+              `uvm_info(get_type_name(),"\n Scoreboard half duplex comparison failed", UVM_LOW)
+              end
+              end
+              else begin
+                if((uart1.Pwdata[7:0]==uart1.Prdata[7:0]))
+                  `uvm_info(get_type_name(),"\n Scoreboard loopback comparison passed",UVM_LOW)
+                  else
+                    `uvm_info(get_type_name(),\n"Scoreboard loopback comparison failed",UVM_LOW)
+                    end
 
-          
-  covergroup apb_signals_cov;
-    option.per_instance=1;
+                    end
 
-    ADDRESS: coverpoint cov_data1.Paddr {bins add={[0:255]};}
-    WR_ENB: coverpoint cov_data1.Pwrite {bins rd={0};
-                                         bins wr={1};}
-  endgroup
-
-  
+                    if((uart1.iir
