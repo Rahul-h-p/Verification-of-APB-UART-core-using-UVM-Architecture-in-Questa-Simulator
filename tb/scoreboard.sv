fe31c51ce5c9786fc26c2cfe0c4c68ebcfbf1729
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-03-08 15:10:34 +0530

    Update scoreboard.sv

--- tb/scoreboard.sv
+++ tb/scoreboard.sv
@@ -48,7 +48,76 @@ covergroup apb_lcr_cov;
                                              bins even_parity=  {1'b1};}
   BREAK: coverpoint cov_data.lcr[6] {bins low={1'b0};
                                      bins high={1'b1};}
-  DIV_LCH: coverpoint 
+  DIV_LCH: coverpoint cov_data.lcr[7] {bins low = {1'b0};
+                                       bins high={1'b1};}
+  LCR_RST: coverpoint cov_data.lcr[7:0] {bins lcr_rst ={8'd3};}
+  cross_cover: cross CHAR_SIZE,STOP_BIT,  PARITY,  Ev_odd_parity,  STICK_PARITY, BREAK,  DIV_LCH, LCR_RST;
+endgroup
+
+  covergroup apb_ier_cov;
+    option.per_instance=1;
+    RCVD_INT: coverpoint cov_data1.ier[0] { bins dis={1'b0};
+                                           bins en={1'b1};}
+    THRE_INT: coverpoint cov_data1.ier[1] { bins dis = {1'b0};
+                                           bins en={1'b1};}
+    LSR_INT: coverpoint cov_data1.ier[2] { bins dis = {1'b0};
+                                           bins en={1'b1};}
+    IER_INT: coverpoint cov_data1.ier[7:0] { bins ier_rst = {8'd0};}
+  endgroup
+
+  covergroup apb_fcr_cov;
+    option.per_instance=1;
+
+    RFIFO: coverpoint cov_data1.fcr[1] {bins dis={1'b0};
+                                          bins clr={1'b1};}
+    TFIFO: coverpoint cov_data1.fcr[2] {bins dis={1'b0};
+                                          bins clr={1'b1};}
+
+    TRG_LVL: coverpoint cov_data1.fcr[7:6] {bins one={2'b00};
+                                            bins four={2'b01};
+                                            bins eight={2'b10};
+                                            bins fouteen={2'b11};
+                                          }
+
+    FCR_RST: coverpoint cov_data1.fcr[7:0] {bins fcr_rst={8'd192};}
+  endgroup
+
+  covergroup apb_mcr_cov;
+    option.per_instance=1;
+    LB: coverpoint cov_data1.mcr[4] {bins dis ={1'b0};
+                                     bins en={1'b1};}
+    MCR_RST: coverpoint cov_data1.mcr[7:0] { bins lcr_rst = {8'd0};}
+  endgroup
+
+  covergroup apb_iir_cov;
+    option.per_instance=1;
+
+    IIR: coverpoint cov_data1.iir[3:1] {bins lsr={3'b001};
+                                        bins rdf={3'b010};
+                                        bins ti_o ={3'b110};}
+    IIR_RST : coverpoint cov_data1.iir[3:0] {bins iir_rst={4'h1};}
+  endgroup
+
+  covergroup apb_lsr_cov;
+    option.per_instance =1;
+
+    DATA_READY: coverpoint cov_data1.lsr[0] {bins fifoempty = {1'b0};
+                                             bins datarcvd= {1'b1};}
+    OVER_RUN: coverpoint cov_data1.lsr[1] {bins nooverrun={1'b0};
+                                           bins overrun ={1'b1};}
+    PARITY_ERR: coverpoint cov_data1.lsr[2] {bins noparityerr ={1'b0};
+                                             bins parityerr ={1'b1};}
+    FRAME_ERR: coverpoint cov_data1.lsr[3] {bins framerr ={1'b0};}
+
+    BREAK_INT: coverpoint cov_data1.lsr[4] {bins nobreakint = {1'b0};
+                                            bins breakint = {1'b1};}
+    b1: coverpoint cov_data1.lsr[5] {bins a1={1'b0};
+                                     bins a2={1'b1};}
+  endgroup
+  
+    
+                                     
+                                         
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
